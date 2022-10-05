4fcc5b995a44baf4b61be59f24606a77ff7ac342
laranja12AAA <110104817+laranja12AAA@users.noreply.github.com>
2022-10-05 09:59:38 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,14 +1,27 @@
 let tabuada = 2;
 
-function escreva(){
+function escreva(){= " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+ 
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+
+
+
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
     document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
     document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
     document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
-}
+    document.write(tabuada + " x 6 
+
+ 
+
+ }
+
+ function cubo(){
+     for(let i = 20; i >= 2; i --){
+     document.write("O Cubo de " + i + " é (" + 1 +" x " + i + " x " + i + " = " + (i*i*i)+"<br>");
+     }
+ }
