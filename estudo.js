3987321c6cf8832a8e2d1116ba509930180eef63
laranja12AAA <110104817+laranja12AAA@users.noreply.github.com>
2022-10-19 10:00:48 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,34 +1,8 @@
-let tabuada = 5;
+function media(){  
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = document.getElementById("v3").value;
+    let r = (Number(n1) + Number(n2) + Number(n3))/3;
 
-function escreva(){
-    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
-}
-
-function minhaTabuada(){
-    for(let i = 10; i >= 1; i--){
-        document.write("Tabuada do " + i + "<br>");
-        for(let j = 1; j <= 10; j++){
-            document.write(i + " x " + j + " = " + (j*i) + "<br>");
-        }
-        document.write("<br>");       
-    }
-}
-
-function cubo(){
-    for(let i = 20; i >= 2; i--){
-        document.write("O cubo de "+i+" é "+i+" x "+i+" x "+i+" = "+(i*i*i)+"<br>");
-    }
-}
-function mat(){
-    let v = document.getElementById ("valor"). Value;
-    let j = document.getElementById ("juros") . Value;
+    document.getElementById("resultado").innerHTML = "Média " + r;
 }
