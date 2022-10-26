b99b658feae5fdc7527e6db5f8166f5d403c98e5
laranja12AAA <110104817+laranja12AAA@users.noreply.github.com>
2022-10-26 09:58:30 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -6,3 +6,16 @@ function media(){
 
     document.getElementById("resultado").innerHTML = "Média " + r;
 }
+
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i =1; i <=t; i++){
+        r = v * (1 + (j/100));
+        document.write("Mês " + i + " - valor: " + r + "<br>");
+        v = r;
+    }
+    document.write("Resultado: " + r) ;
+}    
