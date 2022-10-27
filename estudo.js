24946197a35b7eb0d696cc0f11b1424382cc3ade
laranja12AAA <110104817+laranja12AAA@users.noreply.github.com>
2022-10-27 12:45:29 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -19,3 +19,39 @@ function total(){
     }
     document.write("Resultado: " + r) ;
 }    
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+function mat(){  
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+  
+    if(!Number(v)){
+        alert("O Capitual deve ser numérico.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return
+    }
+    if(!Number(j)){
+        alert("Juros deve ser número.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return
+    }
+    if(!Number(t)){
+        alert("Juros deve ser número.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+    }
+    let r = 0;
+    for(let i=1; i<=t; i++){
+        r = v * (1+(j/100));
+        v = r;
+     // document.write("Mês " + i + " = " + moeda(r) + "<br>");
+     
+    }
+    document.getElementById("totalGeral").innerHTML = "Total: "+moeda(r);
+   // document.write("Resultado " + moeda(r));
+}
